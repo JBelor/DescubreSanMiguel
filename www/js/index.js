575cb5f8be85fbf1ecc38b6aa0d6229a4cd7d137
JBelor <lovobeto18@gmail.com>
2020-04-02 20:23:57 -0600

    splash

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,25 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+(function(){
+    var preload = document.getElementById("preload");
+    var loading = 0;
+    var id = setInterval(frame , 64)
+
+    function frame(){
+        if(loading == 100){
+            clearInterval(id);
+            window.open("principal.html");
+        }else{
+            loading = loading + 1;
+            if(loading == 90){
+                preload.style.animation = "fadeout 1s ease"
+            }
+        }
+    }
+})();
+
+
 var app = {
     // Application Constructor
     initialize: function() {
