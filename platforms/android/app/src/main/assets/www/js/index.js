74fe4033a8944cec410ea34f9bbaecdb2688e3d8
JBelor <lovobeto18@gmail.com>
2020-04-05 09:54:59 -0600

    areglando views

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -16,6 +16,28 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+ /* Parte del spash*/
+(function(){
+    var preload = document.getElementById("preload");
+    var loading = 0;
+    var id = setInterval(frame , 64)
+
+    function frame(){
+        if(loading == 100){
+            clearInterval(id);
+            window.location.href="principal.html";
+        }else{
+            loading = loading + 1;
+            if(loading == 90){
+                preload.style.animation = "fadeout 1s ease"
+            }
+        }
+    }
+})();
+
+
+/*Parte de la app del cordova original*/
 var app = {
     // Application Constructor
     initialize: function() {
