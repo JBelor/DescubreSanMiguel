be41dd8d064c6c8b1b739d77f893c4db094eba6a
JBelor <lovobeto18@gmail.com>
2020-06-02 19:13:05 -0600

    Proyecto arreglado xD

--- www/js/maps.js
+++ www/js/maps.js
@@ -1,12 +1,57 @@
-
 function mapa(){
-    var coord = {lat:13.4562803 ,lng:-88.2041821};
-    var map = new google.maps.Map(document.getElementById('map'),{
-        zoom: 12,
-        center: coord
-    });
+    var divmap = document.getElementById("map");
+    window.navigator.geolocation.getCurrentPosition(fn_ok);
 
+  function fn_ok(rta){
+         var lat = rta.coords.latitude;
+         var lon = rta.coords.longitude;
+
+         var glanLon =new google.maps.LatLng(lat , lon);
+         objConfig = {
+            zoom: 17,
+            center: glanLon
+         }
+         var gmapa = new google.maps.Map(divmap , objConfig) ;
+
+         var marker = new google.maps.Marker({
+            position: glanLon,
+            map: gmapa,
+            title: "Esta es tu posicion"
+         
+        });
+ 
+         console.log(lat);
+         console.log(lon);
+
+    var objconfigDR = {
+      map: gmapa,
+    }
+    var objconfigDS = {
+        origin: glanLon,
+        destination:'Pizza Hut, Metrocentro San Miguel, San Miguel',
+        travelMode:google.maps.TravelMode.DRIVING
+    }
+    
+
+    var ds = new google.maps.DirectionsService(objconfigDS);
+    var dr = new google.maps.DirectionsRenderer(objconfigDR)
+
+    ds.route(objconfigDS , fnRutear);
+
+    function fnRutear(resultado , status){
+    if(status == "OK"){
+        dr.setDirections(resultado);
+    }else{
+        alert("Error! No se pudo trazar la ruta");
+    }
+    }
+  }
 }
+
+/*
+ coord = 13.4653317 , -88.1691289
+   
+*/
 
 
 
